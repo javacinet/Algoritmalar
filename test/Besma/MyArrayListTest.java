7e7cac63e49d3adef0d0fabd23163cccfb7edbc7
Basma Bakirci <144848782+besmabakirci1@users.noreply.github.com>
2024-11-18 19:41:41 +0300

    18Nov2024

--- test/Besma/MyArrayListTest.java
+++ test/Besma/MyArrayListTest.java
@@ -37,4 +37,47 @@ public class MyArrayListTest {
         assertEquals(20, list.get(1));
         assertEquals(2, list.size()); // size() metodu çağrıldı
     }
+
+    @Test
+    public void testRemoveLast(){
+        MyArrayList<Integer> list = new MyArrayList<>();
+        list.addLast(10);
+        list.addLast(20);
+
+        assertEquals(20, list.removeLast());
+        assertEquals(1, list.size());
+        assertEquals(10, list.get(0));
+    }
+
+    @Test
+    public void testRemoveFirst(){
+        MyArrayList<Integer>list = new MyArrayList<>();
+        list.addLast(10);
+        list.addLast(20);
+        list.addLast(30);
+
+        assertEquals(10,list.removeFirst()); // İlk eleman çıkarıldı
+        assertEquals(2, list.size());  // Boyut azaldı
+        assertEquals(20, list.get(0)); // Yeni ilk eleman
+    }
+
+    @Test
+    public void testIsEmpty() {
+        MyArrayList<Integer> list = new MyArrayList<>();
+        assertTrue(list.isEmpty()); // Başlangıçta boş
+
+        list.addLast(10);
+        assertFalse(list.isEmpty()); // Eleman eklendiğinde boş değil
+    }
+    @Test
+    public void testClean() {
+        MyArrayList<Integer> list = new MyArrayList<>();
+        list.addLast(10);
+        list.addLast(20);
+
+        list.clean(); // Liste temizlendi
+        assertEquals(0, list.size());    // Boyut sıfırlandı
+        assertTrue(list.isEmpty());     // Liste artık boş
+    }
+
 }
