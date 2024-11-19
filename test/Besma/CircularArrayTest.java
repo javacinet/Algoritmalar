84cf86ab8f4c526f7b6b3e85abd9b739376fc6f7
Basma Bakirci <144848782+besmabakirci1@users.noreply.github.com>
2024-11-19 17:12:59 +0300

    19Nov2024

---
+++ test/Besma/CircularArrayTest.java
@@ -0,0 +1,63 @@
+package Besma;
+import ogrenciler.Besma.list.MyCircularArray;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+public class CircularArrayTest {
+    @Test
+    public void testAddFirst(){
+        MyCircularArray<Integer> circularArray = new MyCircularArray<>(5);
+        circularArray.addFirst(10);
+        circularArray.addFirst(20);
+
+        assertEquals(2,circularArray.size());
+        assertEquals(20,circularArray.get(0));
+        assertEquals(10,circularArray.get(1));
+    }
+    @Test
+    public void testAddLast(){
+        MyCircularArray<Integer> circularArray = new MyCircularArray<>(5);
+        circularArray.addFirst(10);
+        circularArray.addFirst(20);
+
+        assertEquals(2,circularArray.size());
+        assertEquals(10,circularArray.get(0));
+        assertEquals(20,circularArray.get(1));
+    }
+
+    @Test
+    public void testRemoveFirst (){
+        MyCircularArray<Integer> circularArray = new MyCircularArray<>(5);
+        circularArray.addLast(10);
+        circularArray.addLast(20);
+        int removed = circularArray.removeFirst();
+
+        assertEquals(10, removed);
+        assertEquals(1, circularArray.size());
+        assertEquals(20,circularArray.get(0));
+    }
+
+    @Test
+    public void testRemoveLast(){
+        MyCircularArray<Integer> circularArray = new MyCircularArray<>(5);
+        circularArray.addLast(10);
+        circularArray.addLast(20);
+        int removed = circularArray.removeLast();
+        assertEquals(20,removed);
+        assertEquals(1,circularArray.size());
+        assertEquals(10,circularArray.get(0));
+    }
+
+    @Test
+    public void testResize(){
+        MyCircularArray<Integer> circularArray = new MyCircularArray<>(2);
+        circularArray.addLast(10);
+        circularArray.addLast(20);
+        circularArray.addLast(30); // Kapasiteyi aşar, resize çalışır
+
+        assertEquals(3,circularArray.size());
+        assertEquals(10,circularArray.get(0));
+        assertEquals(20,circularArray.get(1));
+        assertEquals(30,circularArray.get(2));
+
+    }
+}
